f3229a97ceef58a0b290306d082209d418674cf7
CuapioYahir <cuapioatrianoyahir@gmail.com>
2025-04-28 19:19:13 -0600

    Add files via upload

---
+++ RESTA.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+int numa,numb,res;
+cout<<  "ingrese el primer numero: " << endl;
+cin>>numa;
+cout<< "ingrese el segundo numero: " <<endl;
+cin >>numb;
+res=numa-numb;
+cout<< "la suma es de " << res << endl;
+}
